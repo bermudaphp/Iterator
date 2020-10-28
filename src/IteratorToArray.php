b6a6b27ee5a2241fa4e29beb5ed77f67ca145043
Andrey <inbox.shelamkoff@gmail.com>
2020-10-28 13:24:12 +0300

    Create IteratorToArray.php

---
+++ src/IteratorToArray.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Bermuda\Iterator;
+
+
+trait IteratorToArray
+{
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $items = [];
+        
+        foreach ($this as $i => $item)
+        {
+            $items[$i] = $item;
+        }
+        
+        return $items;
+    }
+}
