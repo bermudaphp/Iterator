195e078146eaf5123c802431e951c7f1aec8e9b5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-07-26 01:35:26 +0200

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -7,6 +7,11 @@ use Bermuda\Arrayable;
 function iterableToArray(iterable $iterable): array
 {
     $data = [];
+    
+    if ($iterable instanceof \IteratorAggregate)
+    {
+        return iteratorToArray($iterable->getIterator());
+    }
   
     if ($iterable instanceof Arrayable)
     {
@@ -25,3 +30,21 @@ function iterableToArray(iterable $iterable): array
     
     return $data;
 }
+
+function iteratorToArray(\Iterator $iterator): array
+{
+    $data = [];
+  
+    if ($iterator instanceof Arrayable)
+    {
+        return $iterator->toArray();
+    }
+    
+    foreach($iterator as $k => $v)
+    {
+        $data[$k] = $v;
+    }
+    
+    return $data;
+}
+
